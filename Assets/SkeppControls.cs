af23c184db8cae343b78402af6b9135b201ef203
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2018-10-04 10:35:07 +0200

    Svänga vänster och höger

--- Assets/SkeppControls.cs
+++ Assets/SkeppControls.cs
@@ -2,25 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkeppControls : MonoBehaviour {
-
+public class SkeppControls : MonoBehaviour
+{
+    [Header("ShipControls")]
     public float MovementSpeed;
     public float RotationSpeed;
+    public SpriteRenderer rend;
+    [Header("Timer")]
+    public float TimerCount;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gör så att den konstant åker framåt
         transform.Translate(MovementSpeed * Time.deltaTime, 0, 0, Space.Self);
 
+        //timern tickar uppåt
+        TimerCount += 1 * Time.deltaTime;
+        
+        print(TimerCount);
+
+        //kan svänga höger och bli blå
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, 0, -RotationSpeed * Time.deltaTime);
+            rend.color = Color.blue;
+        }
+        //kan svänga vänster och bli grön
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, RotationSpeed * Time.deltaTime);
+            rend.color = Color.green;
+        }
+        //när man håller inne S så sänks farten med 2
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MovementSpeed /= 2;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            MovementSpeed *= 2;
         }
 
-	}
+
+    }
 }
