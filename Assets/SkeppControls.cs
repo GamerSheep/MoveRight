e2ef49788864d39de2877c9f2db8aff5ac4cbc3c
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2018-10-04 09:23:33 +0200

    Move Right

---
+++ Assets/SkeppControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeppControls : MonoBehaviour {
+
+    public float MovementSpeed;
+    public float RotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(MovementSpeed * Time.deltaTime, 0, 0, Space.Self);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -RotationSpeed * Time.deltaTime);
+        }
+
+	}
+}
