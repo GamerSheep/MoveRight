9bf1b0e79388ad30bee045d58b007e0ecbdc8ad2
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2018-10-08 10:12:08 +0200

    sprite and pulsating colors

--- Assets/SkeppControls.cs
+++ Assets/SkeppControls.cs
@@ -7,7 +7,7 @@ public class SkeppControls : MonoBehaviour
     [Header("ShipControls")]
     public float MovementSpeed;
     public float RotationSpeed;
-    public SpriteRenderer rend;
+    public SpriteRenderer Rend;
     [Header("Timer")]
     public float TimerCount;
     public int TimerIncrease = 1;
@@ -15,16 +15,59 @@ public class SkeppControls : MonoBehaviour
     public int ShipColorNumber;
     public Color ShipColor;
     public SpriteRenderer RandomColor;
-    
+    [Header("Ship")]
+    public Sprite[] SkeppSprite;
+    public int SkeppDecider;
+    [Header("Pulsating")]
+    public float FadeDuration = 1f;
+    public Color PulseColor1;
+    public Color PulseColor2;
+    public Material Material;
+    private Color StartColor;
+    private Color EndColor;
+    private float LastColorChangeTime;
 
     // Use this for initialization
     void Start()
     {
+
+        PulseColor1 = new Color(Random.value, Random.value, Random.value);
+        PulseColor2 = new Color(Random.value, Random.value, Random.value);
+
+        //väljer färgerna för pulseringen
+        Material = GetComponent<Renderer>().material;
+        StartColor = PulseColor1;
+        EndColor = PulseColor2;
+
         //Ger en slumpässigt movement speed
         MovementSpeed = Random.Range(1, 11);
 
         //Gör så att det spawnar på ett slumpmässigt ställe
         transform.Translate(Random.Range(-9, 10), Random.Range(-4, 5), 0);
+
+        //gör så att skeppets sprite blir slumpmässigt
+        SkeppDecider = Random.Range(1, 5);
+        
+        if(SkeppDecider == 1)
+        {
+            GetComponent<SpriteRenderer>().sprite = SkeppSprite[0];
+        }
+        if (SkeppDecider == 2)
+        {
+            GetComponent<SpriteRenderer>().sprite = SkeppSprite[1];
+        }
+        if(SkeppDecider == 3)
+        {
+            GetComponent<SpriteRenderer>().sprite = SkeppSprite[2];
+        }
+        if(SkeppDecider == 4)
+        {
+            GetComponent<SpriteRenderer>().sprite = SkeppSprite[3];
+        }
+
+        
+        
+        
     }
 
     // Update is called once per frame
@@ -46,13 +89,13 @@ public class SkeppControls : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, 0, -RotationSpeed * Time.deltaTime);
-            rend.color = Color.blue;
+          
         }
         //kan svänga vänster och bli grön
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0, 0, RotationSpeed *0.6f * Time.deltaTime);
-            rend.color = Color.green;
+            
         }
         //när man håller inne S så går man hälften så fort
         if (Input.GetKeyDown(KeyCode.S))
@@ -63,13 +106,14 @@ public class SkeppControls : MonoBehaviour
         {
             MovementSpeed *= 2;
         }
-        //nör man trycker på space så får skeppet en slumpmässig färg
+        //när man trycker på space så får skeppet en slumpmässig färg
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ShipColor = new Color(Random.value, Random.value, Random.value, 1.0f);
-            rend.color = ShipColor;
+            Rend.color = ShipColor;
         }
 
+        //gör så att skeppet "warpar" runt skärmen
        if (transform.position.x < -9)
         {
             transform.position = new Vector3(9, transform.position.y);
@@ -85,6 +129,18 @@ public class SkeppControls : MonoBehaviour
         if (transform.position.y > 5)
         {
             transform.position = new Vector3(transform.position.x, -5);
+        }
+        //pulsating colors
+        var ratio = (Time.time - LastColorChangeTime / FadeDuration);
+        ratio = Mathf.Clamp01(ratio);
+        Material.color = Color.Lerp(StartColor, EndColor, ratio);
+        if (ratio == 1f)
+        {
+            LastColorChangeTime = Time.time;
+
+            var temp = StartColor;
+            StartColor = EndColor;
+            EndColor = temp;
         }
     }
 }
